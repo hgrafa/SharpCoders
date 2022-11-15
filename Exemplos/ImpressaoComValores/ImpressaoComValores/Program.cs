c7dc3008855468cb27b5e36174fd8b7f39ed6c16
hgrafa <hugorafell@gmail.com>
2022-11-14 22:38:50 -0300

    Adicionando exemplo: Primeiro Projeto

---
+++ Exemplos/ImpressaoComValores/ImpressaoComValores/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace ImpressaoComValores {
+    public class Program {
+        public static void Main(string[] args) {
+
+            // concatenação
+            int idade = 21;
+            Console.WriteLine("A idade é " + idade);
+
+            // interpolação
+            string nomeDaTurma = "Sharp Coders";
+            int anoDeInicio = 2022;
+            Console.WriteLine($"O nome da turma é {nomeDaTurma} começando em {anoDeInicio}");
+
+        }
+    }
+}
