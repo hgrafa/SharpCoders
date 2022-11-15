099849a36806b1b8b05d921d5c2de8792192a37f
hgrafa <hugorafell@gmail.com>
2022-11-14 22:40:37 -0300

    Atualizando exemplo: Impresão com valores

--- Exemplos/ImpressaoComValores/ImpressaoComValores/Program.cs
+++ Exemplos/ImpressaoComValores/ImpressaoComValores/Program.cs
@@ -1,13 +1,15 @@
-﻿namespace ImpressaoComValores {
-    public class Program {
+﻿namespace ImpressaoComValores 
+{
+    public class Program 
+    {
         public static void Main(string[] args) {
 
             // concatenação
             int idade = 21;
+            string nomeDaTurma = "Sharp Coders";
             Console.WriteLine("A idade é " + idade);
 
             // interpolação
-            string nomeDaTurma = "Sharp Coders";
             int anoDeInicio = 2022;
             Console.WriteLine($"O nome da turma é {nomeDaTurma} começando em {anoDeInicio}");
 
