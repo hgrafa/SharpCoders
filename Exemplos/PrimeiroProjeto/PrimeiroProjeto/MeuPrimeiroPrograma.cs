89c98303d6259688adb2f8ecc4557e7594eaf865
hgrafa <hugorafell@gmail.com>
2022-11-14 22:38:38 -0300

    Adicionando exemplo: Primeiro projeto

---
+++ Exemplos/PrimeiroProjeto/PrimeiroProjeto/MeuPrimeiroPrograma.cs
@@ -0,0 +1,15 @@
+﻿namespace PrimeiroProjeto {
+
+    public class MeuPrimeiroPrograma {
+
+        // método Main - Método Principal
+        
+        public static void Main(string[] args) {
+
+            Console.WriteLine("Olá mundo!");
+            Console.WriteLine("Primeira aula de C#");
+            Console.WriteLine("Testando o console write line!");
+
+        }
+    }
+}
