e60b58e0e0ff0633b0cf5033700bb7865ab180fe
hgrafa <hugorafell@gmail.com>
2022-11-17 12:22:58 -0300

    Adicionando exemplo: Entradas

---
+++ Exemplos/Entradas/Entradas/Program.cs
@@ -0,0 +1,36 @@
+﻿namespace Variaveis {
+
+    public class Program {
+
+        public static void Main(string[] args) {
+
+            Console.Write("Digite o seu nome: ");
+            string nome = Console.ReadLine();
+
+            Console.Write("Digite a sua idade: ");
+            int idade = int.Parse( Console.ReadLine() );
+
+            Console.WriteLine("---------------------------");
+
+            double altura = 1.8;
+            float nota = 8.9F;
+            char sexo = 'M';
+            long identificador = 4L;
+
+            double pi = 3.14159265;
+
+            Console.WriteLine($"Id = {identificador}");
+            Console.WriteLine($"Nome do usuario: {nome}");
+            Console.WriteLine($"Idade: {idade} | Altura: {altura} | sexo: {sexo}");
+            Console.WriteLine($"Nota: {nota}");
+            Console.WriteLine($"O valor de Pi é: {pi:F3}");
+        }
+
+    }
+
+}
+
+// Id = 4
+// Nome do usuario: Joao da Silva
+// Idade: 21 | Altura: 1.8 | sexo: M
+// Nota: 8.9
