dfc074e3edbc50f97d3578c04ce53ad5fb44effc
hgrafa <hugorafell@gmail.com>
2022-11-17 12:24:02 -0300

    Adicionando Exercício: Hello World

---
+++ Exercicios/Beecrowd/HelloWorld_1000/HelloWorld_1000/Program.cs
@@ -0,0 +1,9 @@
+﻿namespace HelloWorld_1000 {
+    public class Program {
+
+        // ponto de entrada do programa - entrypoint
+        public static void Main() {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
