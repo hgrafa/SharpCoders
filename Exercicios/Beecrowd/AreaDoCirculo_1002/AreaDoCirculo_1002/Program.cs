27fe273f8df184ff5c42089ad99cf2005cd570cd
hgrafa <hugorafell@gmail.com>
2022-11-22 08:19:48 -0300

    Adicionando Exercicio: Beecrowd Area do Circulo

---
+++ Exercicios/Beecrowd/AreaDoCirculo_1002/AreaDoCirculo_1002/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace AreaDoCirculo_1002 {
+
+    public class Program {
+
+        public static void Main(string[] args) {
+
+            double PI = 3.14159;
+            double raio;
+
+            raio = double.Parse(Console.ReadLine());
+            double area = PI * raio * raio;
+
+            Console.WriteLine($"A={area:F4}");
+        }
+
+    }
+
+}
+
+// A = 12.56636
+// A = 12.5664
---
+++ Exercicios/Beecrowd/OMaior_1013/OMaior_1013/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace OMaior_1013 {
+
+    public class Program {
+
+        public static void Main(string[] args) {
+
+            string[] numeros = Console.ReadLine().Split();
+            
+            // default/padrão = separar por espaço
+
+            // 2 3 4
+            int n1 = int.Parse(numeros[0]);
+            int n2 = int.Parse(numeros[1]);
+            int n3 = int.Parse(numeros[2]);
+
+            int maior = (n1 + n2 + Math.Abs(n1 - n2)) / 2;
+            
+            maior = (maior + n3 + Math.Abs(maior - n3)) / 2;
+
+            Console.WriteLine($"{maior} eh o maior"); // 106
+        }
+
+    }
+
+}
+
