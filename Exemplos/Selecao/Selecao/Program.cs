fe2652cfd9096ba31c2df4848aca3fcac3dd1c2a
hgrafa <hugorafell@gmail.com>
2022-11-22 08:17:20 -0300

    Adicionando Exemplo: Estruturas de selecao

---
+++ Exemplos/Selecao/Selecao/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Runtime.Intrinsics.X86;
+
+namespace Selecao {
+
+    public class Program {
+
+        public static void Main(string[] args) {
+
+            int idade = int.Parse(Console.ReadLine());
+            bool condicao = (idade > 23);
+
+            if (condicao) {
+                Console.WriteLine("Foi identificada uma idade ok!");
+            }
+
+            if(idade == 25) {
+                Console.WriteLine("Tem 25 anos!");
+            }
+
+            if(idade == 35) {
+                Console.WriteLine("Tem 35 anos!");
+            }
+
+            Console.WriteLine("Fim do programa!");
+
+            // Foi identificada uma idade ok!
+            // Tem 35 anos!
+            // Fim do programa!
+
+        }
+
+    }
+
+}
