326f8d57411aa27600b2d89392698db9555d173f
hgrafa <hugorafell@gmail.com>
2022-11-23 22:21:49 -0300

    Adicionando exemplo: vetores 2

---
+++ Exemplos/Vetores2/Vetores2/Program.cs
@@ -0,0 +1,27 @@
+﻿namespace Vetores2 {
+
+    public class Program {
+
+        public static void Main(string[] args) {
+
+            int tamanho = int.Parse(Console.ReadLine());
+
+            int[] vetor = new int[tamanho];
+
+            for(int i = 0; i < tamanho; i++) {
+                vetor[i] = int.Parse(Console.ReadLine());
+            }
+
+            int soma = 0;
+
+            for(int i = 0; i < tamanho; i++) {
+                soma += vetor[i];
+            }
+
+            Console.WriteLine($"Soma = {soma}");
+
+        }
+
+    }
+
+}
