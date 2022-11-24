bf6a933123d5dbdad0a2ba94670bdc5af9a7da01
hgrafa <hugorafell@gmail.com>
2022-11-23 22:22:39 -0300

    Adicionando solução: Beecrowd 1042

---
+++ Exercicios/Beecrowd/SortSimples_1042/SortSimples_1042/Program.cs
@@ -0,0 +1,39 @@
+﻿namespace SortSimples_1042 {
+
+    public class Program {
+
+        public static void Main(string[] args) {
+
+            string[] numeros = Console.ReadLine().Split();
+
+            int a = int.Parse(numeros[0]);
+            int b = int.Parse(numeros[1]);
+            int c = int.Parse(numeros[2]);
+
+            int d = a;
+            int e = b;
+            int f = c;
+
+            if(d > e && d > f) {
+                int aux = d;
+                d = f;
+                f = aux;
+            } else if(e > d && e > f) {
+                int aux = e;
+                e = f;
+                f = aux;
+            }
+
+            if (d > e) {
+                int aux = d;
+                d = e;
+                e = aux;
+            }
+
+            Console.WriteLine($"{d}\n{e}\n{f}\n\n{a}\n{b}\n{c}");
+
+        }
+
+    }
+
+}
