77c07f080c4c140f1eb6aae4bec05725b6c8c3bc
hgrafa <hugorafell@gmail.com>
2022-11-23 22:21:36 -0300

    Adicionando exemplo: for

---
+++ Exemplos/For/For/Program.cs
@@ -0,0 +1,15 @@
+﻿namespace For {
+
+    public class Program {
+
+        public static void Main(string[] args) {
+
+            for (int count = 1; count <= 10; count++) {
+                Console.WriteLine(count);
+            }
+
+        }
+
+    }
+
+}
