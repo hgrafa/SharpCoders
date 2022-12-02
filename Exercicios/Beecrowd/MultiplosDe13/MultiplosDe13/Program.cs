2a09f03e214288181fb87da863a586e529ad4ee7
hgrafa <hugorafell@gmail.com>
2022-12-02 04:09:33 -0300

    Adicionando solução: Múltiplos de 13 beecrowd

---
+++ Exercicios/Beecrowd/MultiplosDe13/MultiplosDe13/Program.cs
@@ -0,0 +1,32 @@
+﻿namespace MultiplosDe13 {
+
+    public class Program {
+
+        static int SomarDosMultiplosDe13(int inic, int fim) {
+            int soma = 0;
+
+            for (int i = inic; i <= fim; i++) {
+                if (i % 13 != 0) soma += i;
+            }
+
+            return soma;
+        }
+
+        public static void Main (string[] args) {
+        
+            int start = int.Parse(Console.ReadLine());
+            int end = int.Parse(Console.ReadLine());
+
+            if(start > end) {
+                int aux = start;
+                start = end;
+                end = aux;
+            }
+
+            Console.WriteLine( SomarDosMultiplosDe13(start, end) );
+
+        }
+
+    }
+}
+
