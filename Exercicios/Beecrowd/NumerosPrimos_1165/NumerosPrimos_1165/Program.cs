5fc76ef764afe7d413491a131b0532b14938ce6f
hgrafa <hugorafell@gmail.com>
2022-12-02 04:09:48 -0300

    Adicionando solução: Números primos beecrowd

---
+++ Exercicios/Beecrowd/NumerosPrimos_1165/NumerosPrimos_1165/Program.cs
@@ -0,0 +1,35 @@
+﻿namespace NumerosPrimos_1165 {
+
+    public class Program {
+
+        static bool isPrime(int value) {
+
+            if (value == 1) return false;
+
+            for(int i = 2; i < value; i++) {
+                if (value % i == 0)
+                    return false;
+            }
+
+            return true;
+        }
+
+        public static void Main(string[] args) {
+
+            int quantidade = int.Parse(Console.ReadLine());
+
+            for(int i = 0; i < quantidade; i++) {
+                int numero = int.Parse(Console.ReadLine());
+
+                string resp = $"{numero} ";
+                if (!isPrime(numero)) resp += "nao ";
+                resp += "eh primo";
+
+                Console.WriteLine(resp);
+            }
+
+        }
+
+    }
+
+}
