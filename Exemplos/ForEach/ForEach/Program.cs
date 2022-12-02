7d656634642ef9d42d835d49c19d589eb59f6ba5
hgrafa <hugorafell@gmail.com>
2022-12-02 04:08:26 -0300

    Adicionando exemplo: foreach

---
+++ Exemplos/ForEach/ForEach/Program.cs
@@ -0,0 +1,22 @@
+﻿namespace ForEach {
+
+    public class Program {
+
+        public static void Main(string[] args) {
+
+            int[] vetor = { 1, 2, 3, 4 };
+
+            for(int i = 0; i < vetor.Length; i++) {
+                vetor[i] = 2*i;
+            }
+
+            for (int i = 0; i < vetor.Length; i++) {
+                vetor[i] = 2 * i;
+            }
+
+        }
+
+    }
+
+}
+
