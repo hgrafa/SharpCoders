c4be0010bf4c93d9446d0142f00c8c1c86cf917d
hgrafa <hugorafell@gmail.com>
2022-12-02 04:09:16 -0300

    Adicionando solução: Flipper beecrowd

---
+++ Exercicios/Beecrowd/Flipper_2454/Flipper_2454/Program.cs
@@ -0,0 +1,26 @@
+﻿using System.Xml;
+
+namespace Fliper_2454 {
+
+    public class Program {
+
+        public static void Main(string[] args) {
+
+            string[] valores = Console.ReadLine().Split();
+
+            int p = int.Parse(valores[0]);
+            int r = int.Parse(valores[1]);
+
+            if( p == 0 ) {
+                Console.WriteLine("C");
+            } else if( r == 0 ) {
+                Console.WriteLine("B");
+            } else {
+                Console.WriteLine("A");
+            }
+
+        }
+
+    }
+
+}
