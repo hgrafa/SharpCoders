d9bc68b2ea6f0d6c0325b8e91b1e01d7b27e220d
hgrafa <hugorafell@gmail.com>
2022-12-09 07:50:02 -0300

    Adicionando exemplo: Switch case

---
+++ Exemplos/SwitchCase/SwitchCase/Program.cs
@@ -0,0 +1,24 @@
+﻿namespace SwitchCase {
+
+    public class Program {
+
+        static bool IsVowel(char c) {
+            switch(c) {
+                case 'a': 
+                case 'e':
+                case 'i':
+                case 'o':
+                case 'u':
+                    return true;
+                default:
+                    return false;
+            }
+        }
+
+        public static void Main(string[] args) {
+
+        }
+
+    }
+
+}
