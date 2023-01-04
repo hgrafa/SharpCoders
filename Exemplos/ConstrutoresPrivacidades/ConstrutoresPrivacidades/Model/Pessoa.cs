4a9c7168808f402757069253c5811fd850061bf3
hgrafa <hugorafell@gmail.com>
2023-01-04 20:32:28 -0300

    Adicionando exemplo com getters and setters manuais

---
+++ Exemplos/ConstrutoresPrivacidades/ConstrutoresPrivacidades/Model/Pessoa.cs
@@ -0,0 +1,47 @@
+﻿namespace ConstrutoresPrivacidades.Model {
+    public class Pessoa {
+
+        // atributos
+        private string Nome;
+        private double Altura;
+        private int _idade;
+        private string Telefone;
+
+        // construtores
+        public Pessoa(string nomeEntrada, double alturaEntrada, int idadeEntrada) {
+            Nome = nomeEntrada;
+            Altura = alturaEntrada;
+            _idade = idadeEntrada;
+        }
+
+        public Pessoa(string nomeEntrada, double alturaEntrada, int idadeEntrada, string telefoneEntrada) {
+            Nome = nomeEntrada;
+            Altura = alturaEntrada;
+            _idade = idadeEntrada;
+            Telefone = telefoneEntrada;
+        }
+
+        public string GetNome() {
+            return Nome;
+        }
+
+        public void SetNome(string nomeEntrada) {
+            Nome = nomeEntrada;
+        }
+
+        public int GetIdade() {
+            return _idade;
+        }
+
+        public void SetIdade(int idadeEntrada) {
+            _idade = idadeEntrada;
+        }
+
+        // métodos
+        public void FazerAniversario() {
+            _idade++;
+        }
+
+
+    }
+}
---
+++ Exemplos/ConstrutoresPrivacidades/ConstrutoresPrivacidades/Program.cs
@@ -0,0 +1,21 @@
+﻿using ConstrutoresPrivacidades.Model;
+
+namespace ConstrutoresPrivacidades {
+
+    public class Program {
+
+        public static void Main(string[] args) {
+
+            Pessoa marcos = new Pessoa("Marcos Castro", 1.90, 18);
+
+            marcos.Telefone = "1111111111";
+
+            Pessoa julia = new Pessoa("Julia Silva", 1.70, 20, "99999999");
+
+            Console.WriteLine(marcos.PegarIdade());
+
+        }
+
+    }
+
+}
