b40d5ae27ff8cb12deae2b84dbfcf007a0ab6167
hgrafa <hugorafell@gmail.com>
2023-02-06 23:59:43 -0300

    Refatorando pastas

---
+++ Exemplos/Csharp/TestesUnitários/TestesUnitários/Exceptions/DivisionByZeroException.cs
@@ -0,0 +1,11 @@
+﻿namespace ProjetoCalculadora.Exceptions;
+
+public class DivisionByZeroException : Exception {
+    
+    public DivisionByZeroException() {
+    }
+
+    public DivisionByZeroException(string? message) : base(message) {
+    }
+
+}
