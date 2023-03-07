f69f8f956b0fc475cb965f67085155711724eedd
hgrafa <hugorafell@gmail.com>
2023-03-07 18:13:36 -0300

    adding C# complements

---
+++ Exemplos/Csharp/OptionalSubjects/OptionalSubjects/ImplictOperators.cs
@@ -0,0 +1,19 @@
+﻿namespace OptionalSubjects; 
+
+public class ImplictOperators {
+
+    static void Main(string[] args) {
+        var requestModel = new NewUserRequestModel {
+            Username = "admin",
+            Password = "password",
+            Email = "email@gmail.com",
+            Phone = "2"
+        };
+
+        User userToSaveFromForm = requestModel;
+        string viewUser = (UserViewModel) userToSaveFromForm;
+        Console.WriteLine( viewUser );
+            
+    }
+
+}
---
+++ Exemplos/Csharp/Referencia/Referencia/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace Referencia;
+
+public class Program {
+
+    static void Troca(ref int a, ref int b) {
+        int temp = a;
+        a = b;
+        b = temp;
+    }
+
+    static void Divisao(int dividendo, int divisor, out int resultado, out int resto) {
+        resto = dividendo % divisor;
+        resultado = dividendo / divisor;
+    }
+
+    static void Main(string[] args) {
+        int x = 2, y = 3;
+        Troca(ref x, ref y);
+
+        Console.WriteLine($"X = {x} | Y = {y}");
+        Divisao(20, 4, out int resultado, out int resto);
+        Console.WriteLine(resultado);
+    }
+
+}
